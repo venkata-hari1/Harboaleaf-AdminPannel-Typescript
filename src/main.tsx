bbe2724b72b8543eb2b384d53e8030729ecd2cf7
Sanjeev <sanjeev.ksv@gmail.com>
2025-03-12 23:02:35 +0530

    changes to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
+import App from './App.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
